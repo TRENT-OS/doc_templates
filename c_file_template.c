7debedf84142b4f68781d6829bcfc4f3c4e451a3
Slawomir Kwasniak <elgeniusz@gmail.com>
2020-03-03 22:17:19 +0100

    [REF] Improve C files templates' doxygen comments
    1. Functions' arguments are now commented with the doxygen one liner
    comment.
    
    2. Copyright header is not the doxygen comment anymore,
    as otherwise it pollutes doxygen output.

--- c_file_template.c
+++ c_file_template.c
@@ -1,18 +1,10 @@
-/**
- * @copyright (C) 2020, HENSOLDT Cyber GmbH
- *
- * @brief Brief description of the source file.
- *
- * Please notice that if the "@file" param is missing, doxygen will no include
- * this file in the documentation by default unless 'EXTRACT_ALL = YES' config
- * parameter is used.
- */
-#include "c_header_template.h"
+/* Copyright (C) 2020, HENSOLDT Cyber GmbH */
+#include "c_file_template.h"
 #include <stdio.h>
 
 int printFoo(char* foo, int* charWritten)
 {
    *charWritten = printf("%s", foo);
 
-   return charWritten;
+   return *charWritten;
 }
--- c_file_template.h
+++ c_file_template.h
@@ -1,16 +1,21 @@
+/* Copyright (C) 2020, HENSOLDT Cyber GmbH */
+
 /**
- * @copyright (C) 2020, HENSOLDT Cyber GmbH
- *
- * @brief Brief description of the header file
  * @file
+ * @brief Brief description of the header file.
+ *
+ * Please note that if the "@file" param is missing, doxygen will not include
+ * this file in the documentation by default unless 'EXTRACT_ALL = YES' config
+ * parameter is used.
  */
 #pragma once
 
 /**
- * @brief Prints given foo.
- * @param[in]  foo         A C-string to print.
- * @param[out] charWritten The total number of characters written.
- *
+ * @brief  Prints given foo.
  * @return On success, the total number of characters written.
  */
-int printFoo(char* foo, int* charWritten);
+int
+printFoo(
+    char* foo,       /**< [in]  A C-string to print. */
+    int* charWritten /**< [out] The total number of characters written. */
+);
