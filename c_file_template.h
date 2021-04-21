a6741398820ef007e9ff3b4d6dd1c5a8caa78c1d
Slawomir Kwasniak <slawomir.kwasniak@hensoldt-cyber.de>
2021-04-21 20:02:44 +0200

    [FIX] Apply guideline rule doxy_docu_2 (params)
    Parameters are now documented accordingly to the doxygen guidelines.
    
    Fixes:
    SEOS-2647

--- c_file_template.h
+++ c_file_template.h
@@ -39,25 +39,20 @@ isNull(
  * Prints given foo.
  *
  * @return On success, the total number of characters written.
+ *
+ * @param[in]  foo         A C-string to print. This is also a nice place to
+ *                         demonstrate a multiline documentation of the
+ *                         parameter :) Notice that the type of the comment is
+ *                         consistent for all the arguments i.e. next argument
+ *                         is not an inline comment even if its description is
+ *                         short enough.
+ *
+ * @param[out] charWritten The total number of characters written.
  */
 int
 printFoo(
-    char* foo,       /**< [in]  A C-string to print. This is also a nice place
-                                to demonstrate multiline inline documentation of
-                                the parameter :)
-
-                                Note the block comment usage in
-                                this case, due to the issues with astyle.
-
-                                Clearly this comment is already too long, and it
-                                should be considered not inlining or making it
-                                shorter but we are trying to make a point here.
-
-                                Notice that the type of the comment is
-                                consistent for all the arguments i.e. next
-                                argument is also block commented! */
-
-    int* charWritten /**< [out] The total number of characters written. */
+    char* foo,
+    int* charWritten
 );
 
 /**
@@ -65,7 +60,7 @@ printFoo(
  *
  * @note Unfortunately it is not possible to inline document macro's parameters.
  *
- * @param   foo - [in] Character to be printed
+ * @param[in] foo Character to be printed.
  *
  */
 #define PRINT_FOO(foo) do{ int written; printFoo(foo, &written); } while(0);
